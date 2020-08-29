811962049b68949c709c2073bf14f9025e2966c6
Andrew Robinson <andrewjrobinson@gmail.com>
2020-08-29 17:17:49 +1000

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="meeting_timer",
-    version="0.1.3",
+    version="0.2.0",
     author="Andrew Robinson",
     author_email="andrewjrobinson+mt@gmail.com",
     description="A simple application to allow you to keep your meetings or webinars running on time.",
